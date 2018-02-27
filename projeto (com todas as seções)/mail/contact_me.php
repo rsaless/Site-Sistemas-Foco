db132eac99ff7f5e6e626652f0b35a7be7590728
Rafael <rspavarina@gmail.com>
2018-02-27 17:31:11 -0300

    Termino dos estudos, início da modificação
    Bugs do serviço de email corrigidos
    Bugs do layout mobile corrigidos
    
    * Início das modificações e adição de conteúdo
    * cópia com o projeto completo criado (para utilização das seções desnecessárias)
    * continuidade apenas ao projeto com seções necessárias, outras seções serão removidas

---
+++ projeto (com todas as seções)/mail/contact_me.php
@@ -0,0 +1,20 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+	echo "No arguments Provided!";
+	return false;
+}
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'rspavarina@sisfoco.com.br';
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: $email_address\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
